e15320fdf2f612db8d98948469437a276452c1b2
Dvir Profus <61055320+dvir2006@users.noreply.github.com>
2024-02-23 23:36:20 +0200

    as

--- generate_images.py
+++ generate_images.py
@@ -60,10 +60,9 @@ async def generate_languages(s: Stats) -> None:
     progress = ""
     lang_list = ""
     sorted_languages = sorted(
-        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size", 0)
+        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
     )
     delay_between = 150
-    print(sorted_languages)
     for i, (lang, data) in enumerate(sorted_languages):
         color = data.get("color")
         color = color if color is not None else "#000000"
