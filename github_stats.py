372fa1071c027cbf4f95523460645f21821a3382
Dvir Profus <61055320+dvir2006@users.noreply.github.com>
2024-02-29 13:04:54 +0200

    b

--- github_stats.py
+++ github_stats.py
@@ -377,7 +377,7 @@ Languages:
         if "C" in self._languages:
             self._languages["C"]["size"] = 50000
         if "C++" in self._languages:
-            self._languages["C++"]["size"] -= 460024
+            self._languages["C++"]["size"] -= 420024
         # TODO: Improve languages to scale by number of contributions to
         #       specific filetypes
         langs_total = sum([v.get("size", 0) for v in self._languages.values()])
